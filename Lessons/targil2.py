2b515d5208b538a3acafee3a372e73d324a489d5
luzonimrod <luzonimrod@gmail.com>
2021-07-05 07:49:20 +0300

    first commit

---
+++ Lessons/targil2.py
@@ -0,0 +1,14 @@
+Name = "Nimrod Luzon"
+Age = 29
+Email="Luzon@gmail.com"
+print("My name is " +Name +"\n My Age is " + str(Age) + "\n My Email is " + Email)
+#print the name in reverse
+print(Name[::-1])
+#print the age times 3
+print(Age*3)
+#find the name in string and print it
+Sen="Idan Nimrod Dorin Shlomo"
+print(Sen)
+Name2=(Name[0:6])
+print("Does " + Name2 + " is this inside the Sentence?\n")
+print(Name2 in Sen)
